465ce1aacdc9e2f7f13cd1ee746d61ad20ce3d23
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-22 15:59:47 -0500

    Complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+
+    while countdown >  0:
+        print(countdown)
+        countdown -= 1
+        if countdown == 0:
+            print('Happy New Year!')
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+   squared = [int * int for int in int_list]
+   return squared
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print ("FizzBuzz")
+        elif i % 3 == 0:
+            print ("Fizz")
+        elif i % 5  == 0:
+            print ("Buzz")
+        else:
+            print(i)
+
